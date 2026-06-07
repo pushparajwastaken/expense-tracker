d265f4e6e2d93bf2e6bdc91b7d9b8d91f31875a5
Pushparaj <psinghparmar42@gmail.com>
2026-06-07 15:03:26 +0530

    check ssession

--- src/lib/checkSession.ts
+++ src/lib/checkSession.ts
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../app/api/auth/[...nextauth]/route";
 export async function getSession() {
   const session = await getServerSession(authOptions);
+  console.log("SESSION", session);
   if (!session?.user?.id) {
     throw new Error("Unauthorized");
   }

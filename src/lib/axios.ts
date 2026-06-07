e85c0a369966e5da336e72290b0f87e25190ba97
Pushparaj <psinghparmar42@gmail.com>
2026-06-07 15:09:10 +0530

    change axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,18 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
-});
-
-API.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-  return config;
 });
 
 export default API;
